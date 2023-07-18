da057e751f20c9d9b7073f77abc00e982a7d1dab
Andy Gomez <45979588+Koato@users.noreply.github.com>
2023-07-18 04:01:28 -0500

    proyecto inicial

---
+++ src/main/java/com/klagan/retows/RetowsApplication.java
@@ -0,0 +1,13 @@
+package com.klagan.retows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klagan/retows/RetowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klagan.retows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
